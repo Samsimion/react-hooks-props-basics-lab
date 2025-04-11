17296363a61247927562d354079d656c0e08721c
simion samuel <samuel.simion@student.moringaschool.com>
2025-04-11 12:57:58 +0300

    props

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,22 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+import Links from "./Links";
 console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home  name="Liza" city="New York" color="firebrick"/>
+      <About bio={user.bio} />
+      <Links github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
-}
+} 
 
 export default App;
